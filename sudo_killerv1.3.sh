b8d181cef47c69ed8cec3b412afce711ebcb3f20
David B <44925796+TH3xACE@users.noreply.github.com>
2019-07-15 23:23:45 +0200

    Update sudo_killerv1.3.sh

--- sudo_killerv1.3.sh
+++ sudo_killerv1.3.sh
@@ -255,7 +255,7 @@ checkcve()
   echo -e "${BOLD}${GREEN}[+] Sudo version vulnerable to the below CVEs:${RESET}"
   sver_tmp=`sudo -V 2>/dev/null| grep "Sudo version" 2>/dev/null | cut -d" " -f 3 2>/dev/null`
   sver=$(echo $sver_tmp | tr -d ' ' | sed 's/P/p/g')
-  cat cve.sudo2.txt | grep "$sver_tmp" | cut -d"+" -f 1,2
+  cat cve.sudo2.txt | grep "$sver_tmp" | cut -d"+" -f 1,2 | awk '{print $0,"\n"}'
   echo -e "\n"
   #cat cve.sudo.txt | while read line
   #do
