e2d3d3abed5cd303cbe10fa688e9b50cafe07b24
TH3xACE <adblue2017@gmail.com>
2019-07-19 20:15:01 +0000

    bug correction

--- SUDO_KILLERv1.3.2.sh
+++ SUDO_KILLERv1.3.2.sh
@@ -964,7 +964,7 @@ if [ "$var2" ]; then
   echo -e "[+] Sudo $1, was found "
   echo -e "Run the following commands :"
   
-  resgrep=`echo '' | cat bins.txt | grep "$1"`
+  resgrep=`echo '' | cat exploits/"$1".txti | grep -v "\* Sudo \*"`
   echo -e "$resgrep"
   
 echo -e "\n"
