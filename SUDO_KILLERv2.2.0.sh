45e7e0040ac12f6d86725fc9934503f881dcd2dd
David B <44925796+TH3xACE@users.noreply.github.com>
2021-05-30 01:07:52 +0200

    Update SUDO_KILLERv2.2.0.sh

--- SUDO_KILLERv2.2.0.sh
+++ SUDO_KILLERv2.2.0.sh
@@ -126,10 +126,10 @@ echo -e "\n"
 if [ "$sudopass" ]; then 
   #echo -e "${RED} [+] Please enter the password of the current user: ${RESET}"
   #read -s userpassword
-  #echo -n "${RED} [+] Please enter the password of the current user: ${RESET}"
-  echo -n "[+] Please enter the password of the current user: "
+  echo -n "${RED} [+] Please enter the password of the current user: ${RESET}"
+  #echo -n "[+] Please enter the password of the current user: "
   echo -e "\n"
-  read -s $userpassword
+  read -s userpassword
   echo "*********"
   cmdwp=`echo $userpassword | sudo -S -l -k 2>/dev/null`
 else 
