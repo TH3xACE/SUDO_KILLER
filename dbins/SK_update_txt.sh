14152df02fae6de23a5bc4a4a3c2f7e1e5b2e477
TH3xACE <adblue2017@gmail.com>
2024-01-25 18:47:58 +0000

    Update dbins and update script

--- dbins/SK_update_txt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat $1 | while read LINE; do
-curl -s https://gtfobins.github.io/gtfobins/"$LINE"/#sudo | html2text | grep "\* Sudo \*" -A 30 > "$LINE".txt
-done 
---
+++ dbins/update/SK_dbins_update.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#file=$1
+file=$(ls | grep bins)
+tempf="conca-temp.txt"
+tempu="conca-new.txt"
+
+toupdate="dbinstoupdate.txt"
+
+line_count_old=$(cat "$file" | grep ".md" | wc -l)
+#line_count_old=$(wc -l < "$file")
+
+FILENAME="bins_$(date +'%d-%m-%y').txt"
+
+
+
+  if [ $file != $FILENAME ]; then
+
+        #curl -s "https://api.github.com/repos/GTFOBins/GTFOBins.github.io/contents/_gtfobins" | jq -r '.[].name' > "$FILENAME"
+        curl -s "https://api.github.com/repos/GTFOBins/GTFOBins.github.io/contents/_gtfobins" | grep -oP '"name": "\K[^"]+'  | grep ".md" >  "$FILENAME"
+
+        cat "$FILENAME" >> "$tempf"
+        cat "$file" >> "$tempf"
+        cat "$tempf" | sort -u > "$tempu"
+      
+        #line_count_new=$(wc -l < "$FILENAME")
+        line_count_new=$(cat "$tempu" | grep ".md" | wc -l)
+
+        if [ "$line_count_new" -gt "$line_count_old" ]; then
+            new_add=$((line_count_new - line_count_old - 1))
+            echo "[+] There are $new_add additions:"
+
+            grep -v -F -f "$FILENAME" "$tempu"  | sed 's/\.md//g' >> "$toupdate"
+            grep -v -F -f "$file" "$tempu"  | sed 's/\.md//g' >> "$toupdate"
+            cat "$toupdate" | sort -u
+
+            rm "$tempf"
+            rm "$tempu"
+
+            mv $file old/
+
+            echo "[+] Downloading new additions in the directory newAdd"
+
+            cat $toupdate | sort -u | while read LINE; do
+            #pline=$( LINE | sed 's/.md//g' )
+            # 
+            #curl -s https://gtfobins.github.io/gtfobins/"$LINE"/#sudo | html2text | grep "\* Sudo \*" -A 30 > newAdd/"$LINE".txt
+            #curl -s https://gtfobins.github.io/gtfobins/"$LINE"/#sudo | html2text | awk '/SUID/{found=1; next} !found {print}' > newAdd/"$LINE".txt
+            
+            curl -s https://gtfobins.github.io/gtfobins/"$LINE"/#sudo | html2text | grep "\* Sudo \*" -A 1000  | awk '/SUID/{found=1; next} !found {print}' > newAdd/"$LINE".txt
+            #echo $LINE
+
+            done 
+
+            echo "[+] Moving the new additions in the directory dbins"
+            mv newAdd/*.txt ../
+
+            rm "$toupdate"
+
+        else
+            echo "[-] The are no additions, please check again later!"
+        
+        fi
+
+else
+ echo "[+] Update Already done today!"
+fi
+
+
+
